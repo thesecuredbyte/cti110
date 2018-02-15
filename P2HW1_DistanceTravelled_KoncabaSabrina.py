65c602cf04d3b949a6c29d22c531034625a95b60
thesecuredbyte <36523442+thesecuredbyte@users.noreply.github.com>
2018-02-15 18:58:28 -0500

    Add files via upload

---
+++ P2HW1_DistanceTravelled_KoncabaSabrina.py
@@ -0,0 +1,29 @@
+# CTI-110
+# P2HW1 - Distance Travelled
+# Sabrina Koncaba
+# 2/15/2018
+
+print ("In this program you will calculate the distane travelled")
+print ("when given a set speed.  The distance will change based")
+print ("upon the amount of time that passes.  In this example")
+print ("the distance will be calculated in 6, 10 and 15 hours")
+print ("based upon a 70 miles per hour MPH.")
+print ("Then the results will be printed.")
+
+
+speed = 70
+time = 6
+distance = speed * time
+
+# Calculation after 6 hours
+print("Distance after 6 hours",distance)
+
+# Calculation after 10 hours
+time = 10
+print("Distance after 10 hours", distance)
+
+# Calculation after 15 hours
+time = 15
+print ("Distance after 15 hours", distance)
+
+
